e83c4547acc833fee670a95e2e8e7b4ac3a3a97a
globalq64 <globalq@naver.com>
2021-08-02 17:00:47 +0900

    secong commit

--- index.js
+++ index.js
@@ -2,10 +2,32 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
 const mongoose = require('mongoose')
 mongoose.connect('', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected ...')).catch(err => console.log(err))
 
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })) //클라이언트정보를 사용할 수 있도록 중계
+
+// application/json
+app.use(express.json())
+
 app.get('/', (req, res) => res.send('Hello WOrld ... '))
+app.post('/register', (req, res) => {
+
+    //회원가일 정보를 가져오면, 데이터 베이스에 넣어준다    
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
+
 app.listen(port, () => console.log(`Example App listening on port ${port}`))
