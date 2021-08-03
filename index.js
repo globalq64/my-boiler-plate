67b81b4d0e5aa3cef4afde12c564edc784776fd4
globalq64 <globalq@naver.com>
2021-08-03 12:42:39 +0900

    03 Path adjusted

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const app = express()
-const port = 5000
 
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 const { User } = require('./models/User')
 
 const mongoose = require('mongoose')
-mongoose.connect('', {
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected ...')).catch(err => console.log(err))
 
+// application/json
+app.use(express.json())
 // application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true })) //클라이언트정보를 사용할 수 있도록 중계
 
-// application/json
-app.use(express.json())
-
 app.get('/', (req, res) => res.send('Hello WOrld ... '))
 app.post('/register', (req, res) => {
-
+    
     //회원가일 정보를 가져오면, 데이터 베이스에 넣어준다    
     const user = new User(req.body)
     user.save((err, userInfo) => {
@@ -27,7 +27,8 @@ app.post('/register', (req, res) => {
             success: true
         })
     })
-
+    
 })
 
+const port = 5000
 app.listen(port, () => console.log(`Example App listening on port ${port}`))
--- models/User.js
+++ models/User.js
@@ -31,4 +31,4 @@ const userSchema = mongoose.Schema({
 })
 
 const User = mongoose.model('User', userSchema)
-module.exports = { User }
+module.exports =  {User}
